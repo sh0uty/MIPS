c383fb3a16591b2b0789b6a22ef4c0e49a10fa7a
Lukas Stolz <lukas.stolz@stud.hshl.de>
2021-06-17 01:04:35 +0200

    Fix and added some logging

--- data_memory.cpp
+++ data_memory.cpp
@@ -2,6 +2,7 @@
 
 void MIPS::data_memory::read(){
     if(MemRead.read() == SC_LOGIC_1){
+        cout << "Read:" << data_mem[address.read().range(6,2).to_uint()] << " From data_mem at:" << address.read().range(6,2).to_uint() << endl; 
         read_data.write(data_mem[address.read().range(6,2).to_uint()]);
     }
     else{
@@ -11,6 +12,7 @@ void MIPS::data_memory::read(){
 
 void MIPS::data_memory::write(){
     if(MemWrite.read() == SC_LOGIC_1){
-        data_mem[address.read().range(6,2).to_uint()].write(write_data);
+        cout << "Write:" << write_data.read() << " Into data_mem at:" << address.read().range(6,2).to_uint() << endl;
+        data_mem[address.read().range(6,2).to_uint()] = write_data.read();
     }
 }
