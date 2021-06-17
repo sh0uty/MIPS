d9073b515c1844b0f11c724ef4f0c0ddca1423d7
Kali <lukeom6@web.de>
2021-06-17 05:40:47 -0400

    pc.cpp and pc.h

---
+++ pc.cpp
@@ -0,0 +1,13 @@
+#include "pc.h"
+
+void MIPS::pc::load_address(){
+
+    current_address.write(address);
+
+}
+
+void MIPS::pc::save_address(){
+
+    address = address_to_load.readd();
+
+}
---
+++ pc.h
@@ -0,0 +1,34 @@
+#ifndef _PC_H
+#define _PC_H
+
+#include "systemc.h"
+
+namespace MIPS {
+
+    SC_MODULE(pc){
+
+        sc_in_clk clk;
+        sc_in<sc_lv<32>> address_to_load;
+        sc_out<sc_lv<32>> current_address;
+
+        sc_lv<32> address;
+
+        void load_address();
+        void save_address();
+
+        SC_CTOR(pc){
+
+            address = "00000000000000000000000000000000"
+
+            SC_METHOD(load_address);
+            sensitive << clk.pos() << clk.neg();
+
+            SC_METHOD(save_address);
+            sensitive << clk.neg();
+
+        };
+    };
+
+}
+
+#endif
